cbb8953e89a6707b620b8ac4d4076731d5ecd2a7
Arin Bryan <arinbryan27@gmail.com>
2023-12-04 12:13:06 +0000

    Refactored to use custom array struct combining 1D/2D/3D array access methods. Also removed redundant methods from PackedArrayFuncs

--- Assets/ScriptReference/ArrayFuncs.cs
+++ Assets/ScriptReference/ArrayFuncs.cs
@@ -1,21 +1,13 @@
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using UnityEngine;
 
-public static class ArrayFuncs
+public static class PackedArrayFuncs
 {
-    public static void edit1DArrayAs2D<T>(ref T[] arr, T value, int x, int y, int arrW, int arrH)
-    {
-        T[,] values = array1Dto2D(arr, arrW, arrH);
-        values[x,y] = value;
-        arr = array2Dto1D(values);
-    }
-
     //FIXME:    This calculates the valid indicies incorrectly, for the time being, the above function is replacing this.
     //          It has no support for brushes larger than 1, so this needs doing sometime soon.
     //          In the mean time, a workaround is to just have a really large value to paint with, and let diffusion do its hting
-    public static string paintTo1DArrayAs2D<T>(ref T[] arr, T value, int x, int y, int arrW, int arrH, int brushSize)
+    public static string paintTo1DArrayAs2D<T>(ref PackedArray<T> arr, T value, int x, int y, int arrW, int arrH, int brushSize)
     {
         int a = (brushSize - 1) / 2;
         List<int> validIndicies = new List<int>(brushSize * brushSize);
@@ -24,7 +16,7 @@ public static class ArrayFuncs
                 int index = (y + i) + (x + j) * arrW;
                 if (y + i >= arrW || y + i < 0) { continue; }
                 if (x + j >= arrH || x + j < 0) { continue; }
-                if (index > arr.Length) { continue; }
+                if (index > arr.length) { continue; }
                 validIndicies.Add(index);
             }
 
@@ -33,47 +25,17 @@ public static class ArrayFuncs
             arr[index] = value;
         }
 
-        return printArray2D(array1Dto2D(arr, arrW + 2, arrH + 2));
+        return printArray2D(arr);
     }
 
-    public static T[] array2Dto1D<T>(T[,] A)
+    public static string printArray2D<T>(PackedArray<T> matrix)
     {
-        int len = A.GetLength(0) * A.GetLength(1);
-        T[] ret = new T[len];
-        for (int i = 0; i < A.GetLength(0); i++) for (int j = 0; j < A.GetLength(1); j++)
-            {
-                ret[i + j * A.GetLength(0)] = A[i, j];
-            }
-        return ret;
-    }
-
-    public static T[,] array1Dto2D<T>(T[] A, int width, int height)
-    {
-        int len = width * height;
-        if (len != A.Length)
-        {
-            StringBuilder ExceptionMessage = new StringBuilder();
-            ExceptionMessage.Append("Expected length of A to be equal to width*height. A's lenth was: ");
-            ExceptionMessage.Append(A.Length.ToString());
-            ExceptionMessage.Append(" width*height was: ");
-            ExceptionMessage.Append((width * height).ToString());
-            Debug.LogError(ExceptionMessage.ToString());
-        }
-        T[,] ret = new T[width, height];
-        for (int i = 0; i < width; i++) for (int j = 0; j < height; j++)
-            {
-                ret[i, j] = A[i + j * width];
-            }
-        return ret;
-    }
-
-    public static string printArray2D<T>(T[,] matrix)
-    {
+        if (matrix.dimensions.Length != 2) { return ""; }
         StringBuilder stringBuilder = new StringBuilder();
         int longestChar = 0;
-        for (int row = 0; row < matrix.GetLength(0); row++)
+        for (int row = 0; row < matrix.dimensions[0]; row++)
         {
-            for (int column = 0; column < matrix.GetLength(1); column++)
+            for (int column = 0; column < matrix.dimensions[1]; column++)
             {
                 if (matrix[row, column].ToString().Length > longestChar)
                 {
@@ -82,10 +44,10 @@ public static class ArrayFuncs
 
             }
         }
-        int xPadding = matrix.GetLength(1) * 2 + matrix.GetLength(1) * longestChar;
-        for (int xDisplay = 0; xDisplay < matrix.GetLength(0); xDisplay++)
+        int xPadding = matrix.dimensions[1] * 2 + matrix.dimensions[1] * longestChar;
+        for (int xDisplay = 0; xDisplay < matrix.dimensions[0]; xDisplay++)
         {
-            for (int yDisplay = 0; yDisplay < matrix.GetLength(1); yDisplay++)
+            for (int yDisplay = 0; yDisplay < matrix.dimensions[1]; yDisplay++)
             {
                 if (yDisplay == 0)
                 {
@@ -108,7 +70,7 @@ public static class ArrayFuncs
 
                 stringBuilder.Append(matrix[xDisplay, yDisplay].ToString());
                 stringBuilder.Append(" ");
-                if (yDisplay == matrix.GetLength(1) - 1)
+                if (yDisplay == matrix.dimensions[1] - 1)
                 {
                     stringBuilder.Append(" ");
                 }
@@ -121,44 +83,22 @@ public static class ArrayFuncs
         }
         return stringBuilder.ToString();
     }
-
-    public static T accessArray1DAs2D<T>(int x, int y, int width, int height, in T[] array)
+    public static void getSlice2D<T>(in PackedArray<T> values, int xStart, int yStart, int xEnd, int yEnd, out PackedArray<T> ret)
     {
-        //if (x >= width || x < 0) { Debug.LogError("Attempted 1D array access using out-of-bounds 2D Coordinates"); }
-        //if (y >= height || y < 0) { Debug.LogError("Attempted 1D array access using out-of-bounds 2D Coordinates"); }
-        return array[x + y * width];
-    }
-    public static T accessArray2Das1D<T>(int index, in T[,] array)
-    {
-        int xLen = array.GetLength(0);
-        int yLen = array.GetLength(1);
-        if (index > (xLen - 1) * (yLen - 1)) { Debug.Log("Attempted 2D array access using out-of-bounds 1D Index"); }
-        int x = index % (xLen - 1);
-        int y = index / (xLen - 1);
-        return array[x, y];
-    }
-    public static int accessArray1DAs2D(int x, int y, int width, int height)
-    {
-        return x + y * width;
-    }
-    public static T[] getSlice1DFromArray1D<T>(in T[] values, int start, int length)
-    {
-        return values.Skip(start).Take(length).ToArray();
-    }
-    public static void getSlice2DFromArray2D<T>(in T[,] values, int xStart, int yStart, int xEnd, int yEnd, out T[,] ret)
-    {
-        int width = values.GetLength(0);
-        int height = values.GetLength(1);
-        ret = new T[xEnd - xStart, yEnd - yStart];
+        if (values.dimensions.Length != 2) { ret = new PackedArray<T>(new int[]{ 0 });  return; }
+        int width = values.dimensions[0];
+        int height = values.dimensions[1];
+        ret = new PackedArray<T>(new int[]{ xEnd - xStart, yEnd - yStart });
         for (int i = xStart; i < xEnd; i++) for (int j = yStart; i < yEnd; j++)
             {
                 ret[i - xStart, j - yStart] = values[i, j];
             }
     }
-    public static T[,] scaleArray2D<T>(T[,] arr, int scale)
+    public static PackedArray<T> scaleArrayAs2D<T>(PackedArray<T> arr, int scale)
     {
-        T[,] dest = new T[arr.GetLength(0) * scale, arr.GetLength(1) * scale];
-        for (int inputX = 0; inputX < arr.GetLength(0); inputX++) for (int inputY = 0; inputY < arr.GetLength(1); inputY++)
+        if (arr.dimensions.Length != 2) { return new PackedArray<T>(new int[] { 0 }); }
+        PackedArray<T> dest = new PackedArray<T>(new int[] { arr.dimensions[0] * scale, arr.dimensions[1] * scale });
+        for (int inputX = 0; inputX < arr.dimensions[0]; inputX++) for (int inputY = 0; inputY < arr.dimensions[1]; inputY++)
             {
                 T item = arr[inputX, inputY];
                 for (int outputX = 0; outputX < scale; outputX++) for (int outputY = 0; outputY < scale; outputY++)
@@ -170,19 +110,44 @@ public static class ArrayFuncs
             }
         return dest;
     }
-    public static T[] scaleArray1Das2D<T>(T[] arr, int scale, int originalWidth, int originalHeight)
+}
+
+public struct PackedArray<T> : IEnumerable<T>
+{
+    public T[] data;
+    public int length;
+    public int[] dimensions;
+    public PackedArray(int[] Dimensions)
     {
-        T[] dest = new T[originalHeight * scale * originalWidth * scale];
-        for (int inputX = 0; inputX < originalWidth; inputX++) for (int inputY = 0; inputY < originalHeight; inputY++)
-            {
-                T item = arr[inputX + inputY * originalWidth];
-                for (int outputX = 0; outputX < scale; outputX++) for (int outputY = 0; outputY < scale; outputY++)
-                    {
-                        int destX = inputX * scale + outputX;
-                        int destY = inputY * scale + outputY;
-                        dest[destX + destY * originalWidth * scale] = item;
-                    }
-            }
-        return dest;
+        this.dimensions = Dimensions;
+        this.length = 1;
+        foreach (int dim in Dimensions)
+        {
+            this.length *= dim;
+        }
+        data = new T[this.length];
+    }
+    public T this[int i]
+    {
+       get { return data[i]; }
+       set { data[i] = value; }
+    }
+    public T this[int i, int j]
+    {
+        get { return data[i + j * dimensions[0]]; }
+        set { data[i + j* dimensions[0]] = value; }
+    }
+    public T this[int i, int j, int k]
+    {
+        get { return data[i + j * dimensions[0] + k * dimensions[0] * dimensions[1]]; }
+        set { data[i + j * dimensions[0] + k * dimensions[0] * dimensions[1]] = value; }
+}
+    public IEnumerator<T> GetEnumerator()
+    {
+        return (IEnumerator<T>)data.GetEnumerator();
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return data.GetEnumerator();
     }
 }
