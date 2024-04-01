a275ef7454df6ce81b6f4e68048c07a19ef63931
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2024-04-01 22:55:29 +0100

    Fixed Menus

--- Assets/UIElements/MenuManager.cs
+++ Assets/UIElements/MenuManager.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class MenuManager : MonoBehaviour
